21c1a1615f086eec87f89e25444496a05052ba60
napolonia <n.apolonia@gmail.com>
2016-04-19 17:13:12 +0200

    Adding Tahoe Service as Container in docker

---
+++ var/local/cDistro/plug/resources/monitor-aas/tahoe.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+INTRODUCER_NAME=""
+INTRODUCER_WEBPORT=""
+INTRODUCER_PUBLIC=""
+INTRODUCER_GRID_FILE=""
+GRID_NAME=""
+
+PACKAGE_FULLNAME="Tahoe-LAFS"
+PACKAGE_NAME=tahoe-lafs
+TAHOE_COMMAND="/usr/bin/tahoe"
+
+DAEMON_USERNAME=tahoe
+DAEMON_GROUP=tahoe
+DAEMON_HOMEDIR="/var/lib/tahoe-lafs"
+DAEMON_SHELL="/usr/sbin/nologin"
+
+TAHOE_INITD_FILE=tahoe-lafs.init.d
+TAHOE_ETC_INITD_FILE=/etc/init.d/tahoe-lafs
+TAHOE_ETC_INITD_FILENAME=tahoe-lafs
+TAHOE_DEFAULT_FILE=tahoe-lafs.etc.default
+TAHOE_ETC_DEFAULT_FILE=/etc/default/tahoe-lafs
+
+INTRODUCER_DIRNAME="introducer"
+INTRODUCER_PUBLIC_FILE="introducer.public"
+INTRODUCER_GRIDNAME_FILE="grid.name"
+INTRODUCER_FURLFILE="introducer.furl"
+WEBPORT_FILENAME="web.port"
+NODE_DIRNAME="node"
+TAHOE_PID_FILE="twistd.pid"
+INTRODUCER_ID="my_nodeid"
+INTRODUCER_PORTFILE="introducer.port"
+TAHOE_CONFIG_FILE=tahoe.cfg
+
+AVAHI_SERVICE_COMMAND=/usr/sbin/avahi-service
+AVAHI_SERVICE_TAHOE=tahoe-lafs
+
+HOST_IP=127.0.0.1
+## Configuration of tahoe-lafs in Cloudy
+## Just call configure INTRODUCER_NAME DAEMON_HOMEDIR INTRODUCER_DIRNAME TAHOE_CONFIG_FILE INTRODUCER_WEBPORT WEBPORT_FILENAME INTRODUCER_PUBLIC INTRODUCER_PUBLIC_FILE GRID_NAME INTRODUCER_GRID_FILE 
+
+random_num() {
+	echo $(shuf -i 100-999 -n 1)
+}
+
+configure_introducer() {
+	### DEFAULTS like in cloudy!
+	RANDOM_NUM=$(random_num)
+	INTRODUCER_NAME=${1:-"MyIntroducer"}
+	INTRODUCER_WEBPORT=${2:-"8228"}
+	INTRODUCER_PUBLIC=${3:-"true"}
+	INTRODUCER_GRID_FILE=${4:-"grid.name"}
+	GRID_NAME=${5:-"Example-Grid-$RANDOM_NUM"}
+
+	sed -i "s/^nickname.*$/nickname = "$INTRODUCER_NAME"/" $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$TAHOE_CONFIG_FILE
+	echo $INTRODUCER_WEBPORT >> $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$WEBPORT_FILENAME
+
+	if [[ "$INTRODUCER_PUBLIC" == "true" ]]; then
+                touch $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_PUBLIC_FILE
+                sed -i "s/^web\.port.*$/web\.port = tcp:"$INTRODUCER_WEBPORT":interface=0.0.0.0/" $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$TAHOE_CONFIG_FILE
+        else
+                sed -i "s/^web\.port.*$/web\.port = tcp:"$INTRODUCER_WEBPORT":interface=127.0.0.1/" $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$TAHOE_CONFIG_FILE
+	fi
+	echo $GRID_NAME >> $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_GRIDNAME_FILE
+
+	if [[ -z "grep -q '^AUTOSTART' $TAHOE_ETC_DEFAULT_FILE" ]]; then
+                sed -i "s/\" /\"/" $TAHOE_ETC_DEFAULT_FILE
+                sed -i "s/\" /\"/" $TAHOE_ETC_DEFAULT_FILE
+                sed -i "s/^AUTOSTART=\"[^\"]*/& introducer /" $TAHOE_ETC_DEFAULT_FILE
+                sed -i "s/ \"/\"/" $TAHOE_ETC_DEFAULT_FILE
+                sed -i "s/ \"/\"/" $TAHOE_ETC_DEFAULT_FILE
+        else
+                echo "AUTOSTART=\"introducer\"" >> $TAHOE_ETC_DEFAULT_FILE
+	fi
+
+	## CHANGING vIP to hIP!
+	if [ -f $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_FURLFILE ]; then
+		#
+		sed -i "s/pb:.*/pb:\/\/"$(cat $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_ID)"@"$HOST_IP":"$(cat $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_PORTFILE)"/introducer" $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_FURLFILE
+	else
+		echo "pb://"$(cat $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_ID)"@"$HOST_IP":"$(cat $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_PORTFILE)"/introducer" > $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_FURLFILE
+	fi
+
+	chown -vR tahoe:tahoe $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME
+
+	## we need to start it so it will create other files
+	/etc/init.d/tahoe-lafs start introducer
+	/etc/init.d/tahoe-lafs stop introducer
+
+	echo "INTRODUCER_PORT=$(cat $DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$INTRODUCER_PORTFILE)"
+}
+
+configure_node() {
+	## Configure node
+	NODE_INTRODUCER_FURL=${1:-""}
+	NODE_NICKNAME=${2:-"MyStorage"}
+	# FURL must have the HOST IP not the container IP
+	# Do we need more configurations?
+	NODE_WEBPORT=${3:-"3456"}
+	NODE_SHARES=${4:-""}
+	NODE_RESERVED=${5:-"1G"}
+
+	sed -i "s/^nickname.*$/nickname = "$NODE_NICKNAME"/" $DAEMON_HOMEDIR/$NODE_DIRNAME/$TAHOE_CONFIG_FILE
+	sed -i "s%^introducer\.furl.*$%introducer\.furl = "$NODE_INTRODUCER_FURL"%" $DAEMON_HOMEDIR/$NODE_DIRNAME/$TAHOE_CONFIG_FILE
+	sed -i "s/^web\.port.*$/web\.port = tcp:"$NODE_WEBPORT":interface=0\.0\.0\.0/" $DAEMON_HOMEDIR/$NODE_DIRNAME/$TAHOE_CONFIG_FILE
+
+	if [[ -z "grep -q '^AUTOSTART' $TAHOE_ETC_DEFAULT_FILE" ]]; then
+		sed -i "s/\" /\"/" $TAHOE_ETC_DEFAULT_FILE
+		sed -i "s/\" /\"/" $TAHOE_ETC_DEFAULT_FILE
+		sed -i "s/^AUTOSTART=\"[^\"]*/& node /" $TAHOE_ETC_DEFAULT_FILE
+		sed -i "s/ \"/\"/" $TAHOE_ETC_DEFAULT_FILE
+		sed -i "s/ \"/\"/" $TAHOE_ETC_DEFAULT_FILE
+	else
+		echo "AUTOSTART=\"node\"" >> $TAHOE_ETC_DEFAULT_FILE
+	fi
+	chown -vR tahoe:tahoe $DAEMON_HOMEDIR/$NODE_DIRNAME
+
+	## we need to start it so it will create other files
+	/etc/init.d/tahoe-lafs start node
+	/etc/init.d/tahoe-lafs stop node
+
+	echo "NODE_PORT=$(cat $DAEMON_HOMEDIR/$NODE_DIRNAME/client.port)"
+}
+
+change() {
+	WHO=$1
+	WHAT=$2
+	TO=$3
+	FILE_TO_CHANGE=""
+
+	if [[ "$WHO" == "node" ]]; then
+	FILE_TO_CHANGE=$DAEMON_HOMEDIR/$NODE_DIRNAME/$TAHOE_CONFIG_FILE
+	elif [[ "WHAT" == "introducer" ]]; then
+	FILE_TO_CHANGE=$DAEMON_HOMEDIR/$INTRODUCER_DIRNAME/$TAHOE_CONFIG_FILE
+	fi
+
+	sed -i "s/^"$WHAT".*$/"$WHAT" = "$TO"/" $FILE_TO_CHANGE
+}
+
+help() {
+
+	echo "Tahoe-LAFS autoconfigure for Cloudy:\n"
+	echo "--> "$0" configure <introducer or node> HOSTIP <ARGS>\n"
+	echo "--> "$0" node change WHAT_TO_CHANGE VALUE\n"
+}
+
+
+case "$1" in 
+ configure)
+	shift
+	if [[ "$1" == "introducer" ]]; then
+		shift
+		HOST_IP=$1
+		shift
+		configure_introducer "$@"
+	elif [[ "$1" == "node" ]]; then
+		shift
+		HOST_IP=$1
+		shift
+		configure_node "$@"
+	fi
+	;;
+ node)
+	shift
+	if [[ "$1" == "change" ]]; then
+		shift
+		change "node" "$@"
+	fi
+	;;
+ *)
+	help
+	exit
+	;;
+esac
---
+++ var/local/cDistro/plug/resources/monitor-aas/tahoe_image.dockerfile
@@ -0,0 +1,59 @@
+#### DOCKER FILE
+##  Test Image building for Peerstreamer service
+##  :: docker build -t <name> - < ps_image.dockerfile
+##  will build the image without working dir, from the dockerfile
+####
+
+FROM philcryer/min-wheezy:latest
+MAINTAINER cloudy@ac.upc.edu
+
+ENV PACKAGE_NAME tahoe-lafs
+RUN apt-get update
+RUN apt-get install -y net-tools
+RUN apt-get install -y rsyslog
+
+## Adding files to container
+RUN mkdir -p /var/local/cDistro/plug/resources/monitor-aas/
+ADD http://10.1.26.2:7000/plug/resources/monitor-aas/common.sh /var/local/cDistro/plug/resources/monitor-aas/common.sh
+
+RUN mkdir -p /var/local/cDistro/plug/resources/tahoe-lafs/
+ADD http://10.1.26.2:7000/plug/resources/tahoe-lafs/tahoe-lafs.init.d /var/local/cDistro/plug/resources/tahoe-lafs/tahoe-lafs.init.d
+ADD http://10.1.26.2:7000/plug/resources/tahoe-lafs/tahoe-lafs.etc.default /var/local/cDistro/plug/resources/tahoe-lafs/tahoe-lafs.etc.default
+## Added to configure tahoe-lafs
+#ADD http://10.1.26.2:7000/plug/resources/monitor-aas/tahoe.sh /var/local/cDistro/plug/resources/monitor-aas/tahoe.sh
+
+
+RUN apt-get install -y tahoe-lafs
+RUN groupadd --system tahoe
+RUN useradd --system -g tahoe --home-dir "/var/lib/tahoe-lafs" --shell "/usr/sbin/nologin" "tahoe"
+RUN mkdir "/var/lib/tahoe-lafs/"
+RUN chown -vR tahoe:tahoe "/var/lib/tahoe-lafs"
+RUN cp -fv /var/local/cDistro/plug/resources/tahoe-lafs/tahoe-lafs.init.d /etc/init.d/tahoe-lafs
+RUN cp -fv /var/local/cDistro/plug/resources/tahoe-lafs/tahoe-lafs.etc.default /etc/default/tahoe-lafs
+RUN chmod -v +x /etc/init.d/tahoe-lafs
+RUN update-rc.d tahoe-lafs defaults
+
+## We should have a IF clause for introducer or node
+#RUN if [ $TAHOE_IN -eq "introducer" ]; then /usr/bin/tahoe create-"$TAHOE_IN" /var/lib/tahoe-lafs/introducer; else /usr/bin/tahoe create-node? /var/lib/tahoe-lafs/node; fi
+RUN mkdir /var/lib/tahoe-lafs/introducer
+RUN mkdir /var/lib/tahoe-lafs/node
+RUN "/usr/bin/tahoe" create-introducer "/var/lib/tahoe-lafs/introducer"
+RUN "/usr/bin/tahoe" create-node "/var/lib/tahoe-lafs/node"
+
+ADD http://10.1.26.2:7000/plug/resources/monitor-aas/tahoe.sh /var/local/cDistro/plug/resources/monitor-aas/tahoe.sh
+
+## Since from here we do not know which will be thefor introducer or node
+# Ill configure for both and just run one of them each time
+RUN /bin/bash /var/local/cDistro/plug/resources/monitor-aas/tahoe.sh configure introducer 10.1.26.2
+RUN /bin/bash /var/local/cDistro/plug/resources/monitor-aas/tahoe.sh configure node 10.1.26.2
+
+# THIS IS EXECd WHEN CREATING CONTAINER!! along with "&& /bin/bash" for container not to stop running
+# /etc/init.d/tahoe-lafs start introducer # for introducer
+# /etc/init.d/tahoe-lafs start node # for storage node
+
+# IF it does not start we can call it with both options worked:
+# tahoe start /var/lib/tahoe-lafs/introducer or node ## --syslog may not be working
+# or twistd -ny /var/lib/tahoe-lafs/introducer/tahoe-introducer.tac
+### ADDING after so IT WILL update the image
+ADD http://10.1.26.2:7000/plug/resources/monitor-aas/tahoe.sh /var/local/cDistro/plug/resources/monitor-aas/tahoe.sh
+
